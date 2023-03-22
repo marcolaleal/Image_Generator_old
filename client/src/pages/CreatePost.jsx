41b46737a55c8dc9bbde0f76db21b4f405bc029e
marcolaleal <marcolaleal@gmail.com>
2023-03-21 23:08:43 -0300

    feat: CreatePost page complete

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -15,17 +15,23 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const generateImage = () => {
+    
+  }
+  
   const handleSubmit = () => {
 
   }
 
   const handleChange = (e) => {
-
+    setForm({...form, [e.target.name]: e.target.value})
   }
 
   const handleSurpriseMe = () =>{
-    
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt })
   }
+
 
   return (
     <section className="max-w-7x1 mx-auto">
@@ -53,13 +59,58 @@ const CreatePost = () => {
             labelName="Prompt:"
             type="text"
             name="Prompt"
-            placeholder="O que deseja criar?"
+            placeholder="Um robo de brinquedo sentado em um muro amarelo"
             value={form.prompt}
             handleChange={handleChange}
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
 
+          <div className="relative bg-gray-50 border border-gray-300
+          text-gray-900 text-sm rounded-lg focus:ring-blue-500 forcus:border-blue-500
+          w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img 
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ):(
+              <img 
+                src={preview}
+                alt="preview"
+                className="w-9/12  h-9/12 object-contain opacity-40"
+              />
+            )}
+
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center 
+              items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div className="mt-5 flex gap-5"> 
+          <button
+            type="button"
+            onClick={generateImage}
+            className="text-white bg-green-700 font-medium rounded-md 
+            text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? "Gerando..." : "Gerado"}
+          </button>
+        </div>
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">Crie uma imageme  compartilhe com outros membros da comunidade</p>
+          <button
+            type='submit'
+            className='mt-3 text-white bg-[#6469ff] font-medium rounded-md 
+            text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+          >
+            {loading ? "Compartilhando..." : "Compartilhar com a comunidade"}
+          </button>
         </div>
       </form>
     </section>
